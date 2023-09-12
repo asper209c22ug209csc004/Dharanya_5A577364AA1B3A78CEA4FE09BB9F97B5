992791ecf05be4e97bfc26071a41babd3c25e9ef
asper209c22ug209csc004 <144773266+asper209c22ug209csc004@users.noreply.github.com>
2023-09-12 12:25:21 +0530

    Add files via upload

---
+++ UNIT 1 CHALLENGE/Fact.py
@@ -0,0 +1,6 @@
+print("PROGRM TO FIND FACTORIAL OF A GIVEN NUMBER")
+def factorial(n):  
+    # single line to find factorial
+    return 1 if (n==1 or n==0) else n * factorial(n - 1)
+num=int(input("Enter a number :"))
+print("Factorial of",num,"is",factorial(num))
